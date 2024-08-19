85c31c7b4e5849a6c9708e6141ab5cb9447d54b6
Huan <phanvanhuan2009@gmail.com>
2024-08-19 10:17:33 +0700

    all

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,22 +29,22 @@ $(function () {
         });
     });
 
-    var bar = new ProgressBar.Line(preloader, {
-        strokeWidth: 1.7,
-        easing: 'easeInOut',
-        duration: 1400,
-        delay: 1000,
-        trailWidth: 1.7,
-        svgStyle: {
-            width: '100%',
-            height: '100%'
-        },
-        step: (state, bar) => {
-            bar.setText(Math.round(bar.value() * 100) + ' %');
-        }
-    });
-
-    bar.animate(1);
+    // var bar = new ProgressBar.Line(preloader, {
+    //     strokeWidth: 1.7,
+    //     easing: 'easeInOut',
+    //     duration: 1400,
+    //     delay: 1000,
+    //     trailWidth: 1.7,
+    //     svgStyle: {
+    //         width: '100%',
+    //         height: '100%'
+    //     },
+    //     step: (state, bar) => {
+    //         bar.setText(Math.round(bar.value() * 100) + ' %');
+    //     }
+    // });
+    //
+    // bar.animate(1);
 
     // counters
     anime({
