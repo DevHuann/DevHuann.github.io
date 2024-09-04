08681bed57501164c9c4326a26598e694a62a86b
Huan <phanvanhuan2009@gmail.com>
2024-09-04 14:48:57 +0700

    all

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,45 +6,45 @@ $(function () {
     "use strict";
 
     // page loading
-    $(document).ready(function () {
-        anime({
-            targets: '.art-preloader .art-preloader-content',
-            opacity: [0, 1],
-            delay: 200,
-            duration: 600,
-            easing: 'linear',
-            complete: function (anim) {
-
-            }
-        });
-        anime({
-            targets: '.art-preloader',
-            opacity: [1, 0],
-            delay: 2200,
-            duration: 400,
-            easing: 'linear',
-            complete: function (anim) {
-                $('.art-preloader').css('display', 'none');
-            }
-        });
-    });
-
-    var bar = new ProgressBar.Line(preloader, {
-        strokeWidth: 1.7,
-        easing: 'easeInOut',
-        duration: 1400,
-        delay: 750,
-        trailWidth: 1.7,
-        svgStyle: {
-            width: '100%',
-            height: '100%'
-        },
-        step: (state, bar) => {
-            bar.setText(Math.round(bar.value() * 100) + ' %');
-        }
-    });
-
-    bar.animate(1);
+    // $(document).ready(function () {
+    //     anime({
+    //         targets: '.art-preloader .art-preloader-content',
+    //         opacity: [0, 1],
+    //         delay: 200,
+    //         duration: 600,
+    //         easing: 'linear',
+    //         complete: function (anim) {
+    //
+    //         }
+    //     });
+    //     anime({
+    //         targets: '.art-preloader',
+    //         opacity: [1, 0],
+    //         delay: 2200,
+    //         duration: 400,
+    //         easing: 'linear',
+    //         complete: function (anim) {
+    //             $('.art-preloader').css('display', 'none');
+    //         }
+    //     });
+    // });
+    //
+    // var bar = new ProgressBar.Line(preloader, {
+    //     strokeWidth: 1.7,
+    //     easing: 'easeInOut',
+    //     duration: 1400,
+    //     delay: 750,
+    //     trailWidth: 1.7,
+    //     svgStyle: {
+    //         width: '100%',
+    //         height: '100%'
+    //     },
+    //     step: (state, bar) => {
+    //         bar.setText(Math.round(bar.value() * 100) + ' %');
+    //     }
+    // });
+    //
+    // bar.animate(1);
 
     // counters
     anime({
